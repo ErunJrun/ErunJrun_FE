46c87f4b783cf97fb6c770a2c8b6002406850a23
Daun1309 <100025580+Daun1309@users.noreply.github.com>
2022-05-02 13:52:10 +0900

    feat_modal

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,7 +1,23 @@
 import React from "react";
+import Weather from "../components/main/Weather";
+import Banner from "../components/main/Banner";
+import MGroupRunning from "../components/main/MGroupRunning";
+import styled from "styled-components";
 
 const Main = () => {
-  return <div>Main</div>;
+  return (
+    <div>
+      <Box>
+        <Banner/>
+        <Weather/>
+      </Box>
+      <MGroupRunning/>
+    </div>
+  );
 };
 
+const Box = styled.div`
+  display: flex;
+  
+`;
 export default Main;
