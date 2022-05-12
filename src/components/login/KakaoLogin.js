fdb4e4db24a5bbe192cf49c30cd990597867aa91
U-Jinyeol <100745412+U-Jinyeol@users.noreply.github.com>
2022-05-12 14:24:48 +0900

    loginTest

--- src/components/login/KakaoLogin.js
+++ src/components/login/KakaoLogin.js
@@ -7,6 +7,7 @@ const KakaoLogin = () => {
 
   // 인가코드
   let code = new URL(window.location.href).searchParams.get("code");
+  console.log(code);
 
   React.useEffect(async () => {
     await dispatch(kakaoLogin(code));
