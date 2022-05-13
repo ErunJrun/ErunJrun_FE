a04a00405d5da5eb04e4f719582afff271cbaf6a
U-Jinyeol <100745412+U-Jinyeol@users.noreply.github.com>
2022-05-14 00:44:47 +0900

    chore_카카오지도 수정

--- src/components/groupDetail/MapInfo.js
+++ src/components/groupDetail/MapInfo.js
@@ -19,27 +19,25 @@ function MapInfo(props) {
     <>
       <Grid margin="0 0 96px 0">
         <Grid>
-          <Map // 지도를 표시할 Container
+          <Map
             id={`map`}
             center={{
-              // 지도의 중심좌표
               lat: detailGroup?.mapLatLng[0]?.lat,
               lng: detailGroup?.mapLatLng[0]?.lng,
             }}
             style={{
-              // 지도의 크기
               width: "758px",
               height: "406px",
             }}
-            level={4} // 지도의 확대 레벨
+            level={4}
             onCreate={setMap}
           >
             <Polyline
               path={detailGroup?.mapLatLng}
-              strokeWeight={5} // 선의 두께입니다
-              strokeColor={"#686EF9"} // 선의 색깔입니다
-              strokeOpacity={1} // 선의 불투명도입니다 0에서 1 사이값이며 0에 가까울수록 투명합니다
-              strokeStyle={"solid"} // 선의 스타일입니다
+              strokeWeight={5}
+              strokeColor={"#686EF9"}
+              strokeOpacity={1}
+              strokeStyle={"solid"}
             />
           </Map>
         </Grid>
