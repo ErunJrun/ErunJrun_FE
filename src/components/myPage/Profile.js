3afbceda43af28b62ec4f9b4ad7e006ccd0af6d5
U-Jinyeol <100745412+U-Jinyeol@users.noreply.github.com>
2022-05-14 00:05:37 +0900

    feat_마이페이지 러닝레벨

--- src/components/myPage/Profile.js
+++ src/components/myPage/Profile.js
@@ -1,60 +1,64 @@
-import React, {useEffect }from 'react';
-import Level from './Level';
+import React, { useEffect } from "react";
+import Level from "./Level";
 import { useSelector } from "react-redux";
 import styled from "styled-components";
-import { Text, Grid } from "../../elements"
+import { Text, Grid } from "../../elements";
+import LevelBox from "../groupDetail/LevelBox";
 
 const Profile = () => {
-
-
   const profile_list = useSelector((state) => state.mypage.list);
- // console.log(profile_list.userInfo.userLevel);
+  // console.log(profile_list.userInfo.userLevel);
   //if (profile_list.length === 0) { return <></>; }
 
+  return (
+    <Box>
+      <MiddleBox>
+        <ImgBox>
+          <MyImage src={profile_list?.userInfo?.profileUrl} />
+          <Text bold size="20px">
+            {" "}
+            {profile_list?.userInfo?.nickname}{" "}
+          </Text>
+        </ImgBox>
 
-  
-    return (
-        <Box>
-            <MiddleBox>
+        <Introduce>
+          {profile_list?.userInfo?.bio}
+          <Triangle />
+        </Introduce>
 
-              <ImgBox>
-                <MyImage src={profile_list?.userInfo?.profileUrl}/>
-                <Text bold size="20px"> {profile_list?.userInfo?.nickname} </Text>
-              </ImgBox>
-
-              <Introduce>
-                 {profile_list?.userInfo?.bio}
-                <Triangle/>
-              </Introduce>
-
-              <Information>
-                  <SmallBox>
-                    <Text bold size="16px"> 러닝레벨 </Text>
-                    <Text 
-                      size="14px"
-                      margin="8px 0 0 0" 
-                      padding="10px 10px 0 7px"
-                      color="#fff"
-                      height="13px"
-                      borderRadius="3px"
-                      bg="#030c37"
-                      textalign="center"
-                    > {profile_list?.userInfo?.userLevel} RUNNER</Text>
-                  </SmallBox>
-                  <SmallBox>
-                    <Text bold size="16px"> 선호거리 </Text>
-                    <Text size="17px" color="#000"> {profile_list?.userInfo?.likeDistance}</Text>
-                  </SmallBox>
-                  <SmallBox>
-                    <Text bold size="16px"> 선호지역 </Text>
-                    <Text size="17px" color="#000"> {profile_list?.userInfo?.likeLocation}</Text>
-                  </SmallBox>
-              </Information>
-
-            </MiddleBox>
-            <Level/>
-        </Box>
-    );
+        <Information>
+          <SmallBox>
+            <Text bold size="16px">
+              {" "}
+              러닝레벨{" "}
+            </Text>
+            <LevelBox userLevel={profile_list?.userInfo.userLevel} />
+          </SmallBox>
+          <SmallBox>
+            <Text bold size="16px">
+              {" "}
+              선호거리{" "}
+            </Text>
+            <Text size="17px" color="#000">
+              {" "}
+              {profile_list?.userInfo?.likeDistance}
+            </Text>
+          </SmallBox>
+          <SmallBox>
+            <Text bold size="16px">
+              {" "}
+              선호지역{" "}
+            </Text>
+            <Text size="17px" color="#000">
+              {" "}
+              {profile_list?.userInfo?.likeLocation}
+            </Text>
+          </SmallBox>
+        </Information>
+      </MiddleBox>
+      <Level />
+    </Box>
+  );
 };
 
 const Box = styled.div`
@@ -67,12 +71,13 @@ const Box = styled.div`
 `;
 
 const MiddleBox = styled.div`
-  display: felx;
-
+  display: flex;
 `;
 
 const SmallBox = styled.div`
-  display: felx;
+  display: flex;
+  align-items: center;
+
   width: 255px;
   height: 23px;
   gap: 20px;
@@ -82,7 +87,7 @@ const SmallBox = styled.div`
 const ImgBox = styled.div`
   width: 100px;
   height: 141px;
-  text-align:center;
+  text-align: center;
   line-height: 0.5;
   margin-top: 8px;
 `;
@@ -90,7 +95,7 @@ const ImgBox = styled.div`
 const MyImage = styled.img`
   height: 100px;
   width: 100px;
-  border-radius: 50%
+  border-radius: 50%;
 `;
 
 const Introduce = styled.div`
@@ -108,19 +113,19 @@ const Introduce = styled.div`
   margin-top: 8px;
 `;
 
-const Triangle= styled.div`
-  width:0;
+const Triangle = styled.div`
+  width: 0;
   height: 0;
   border-style: solid;
   border-width: 1rem;
-  border-color:transparent #e1e1e1 transparent transparent;
+  border-color: transparent #e1e1e1 transparent transparent;
   transform: translate(-95%, 50%);
-`
+`;
 
-const Information= styled.div`
-width: 450px;
-height: 90px;
-line-height: 0.2;
-margin: 7px 0 0 110px;
-`
+const Information = styled.div`
+  width: 450px;
+  height: 90px;
+  line-height: 0.2;
+  margin: 7px 0 0 110px;
+`;
 export default Profile;
