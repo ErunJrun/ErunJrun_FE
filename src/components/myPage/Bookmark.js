0f5d691509a0a7e77121627034ec438cd5790e6c
U-Jinyeol <100745412+U-Jinyeol@users.noreply.github.com>
2022-05-30 13:37:00 +0900

    fix_마이페이지(북마크)

--- src/components/myPage/Bookmark.js
+++ src/components/myPage/Bookmark.js
@@ -15,94 +15,97 @@ const Bookmark = () => {
   console.log(bookmark);
 
   return (
-    <Grid
-      width="1230px"
-      display="flex"
-      margin="0 0 0 -31px"
-    >
-      {bookmark.data.feed.length === 0 ? 
+    <Grid width="1230px" display="flex" margin="0 0 0 -31px">
+      {bookmark.data.feed.length === 0 ? (
         <Box>북마크한 게시물이 없습니다</Box>
-      :
+      ) : (
         <>
           {bookmark?.data?.feed.map((feed, index) => {
             return (
-            <Grid 
-              key={index}
-              width="288px" 
-              display="flex" 
-              alignItems="center" 
-              margin="0 8px"
-              _onClick={() => {
-                history.push(`/courseDetail/${feed?.courseId}`);
-              }}
-            >
               <Grid
-                margin="0 0 90px 0"
+                key={index}
                 width="288px"
                 display="flex"
-                flexDirection="column"
+                alignItems="center"
+                margin="0 8px"
+                _onClick={() => {
+                  history.push(`/courseDetail/${feed?.courseId}`);
+                }}
               >
-                <Grid position="relative">
-                  <img
-                    style={{
-                      width: "288px",
-                      height: "216px",
-                      position: "relative",
-                      borderRadius: "3px 3px 0 0",
-                    }}
-                    src={feed?.courseImageUrl1}
-                  />
-                 {feed?.bookmark ? ( 
-                  <BookmarkWhite
-                    onClick={() => {
-                      dispatch(bookmarkDB(feed?.courseId));
-                    }}
-                    src={bookmarkGreen}
-                  />
-                 ) : ( 
-                  <BookmarkWhite
-                    onClick={() => {
-                      dispatch(bookmarkDB(feed?.courseId));
-                    }}
-                    src={bookmarkWhite}
-                  />
-                 )} 
-
                 <Grid
-                  width="267px"
-                  height="auto"
-                  bg="white"
-                  borderRadius="6px"
-                  padding="10px 16px"
-                  position="absolute"
-                  left="10px"
-                  bottom="-45px"
+                  margin="0 0 90px 0"
+                  width="288px"
+                  display="flex"
+                  flexDirection="column"
                 >
-                  <Text margin="0 0 4px 0" size="14px" bold>
-                    {feed?.title}
-                  </Text>
+                  <Grid position="relative">
+                    <img
+                      style={{
+                        width: "288px",
+                        height: "216px",
+                        position: "relative",
+                        borderRadius: "3px 3px 0 0",
+                      }}
+                      src={feed?.courseImageUrl1}
+                    />
+                    {feed?.bookmark ? (
+                      <BookmarkWhite
+                        onClick={() => {
+                          dispatch(bookmarkDB(feed?.courseId));
+                        }}
+                        src={bookmarkGreen}
+                      />
+                    ) : (
+                      <BookmarkWhite
+                        onClick={() => {
+                          dispatch(bookmarkDB(feed?.courseId));
+                        }}
+                        src={bookmarkWhite}
+                      />
+                    )}
 
-                  <Grid display="flex" alignItems="center" margin="10px 0 8px 0">
-                    <img style={{ width: "13px", height: "17px" }} src={starIcon} />
-                    <Text margin="0 10px 0 4px"size="13px" bold>
-                      {feed?.starPoint}
-                    </Text>
-                    <Text margin="0" color="#909090" size="12px" regular>
-                      리뷰{feed?.commentCnt}개
-                    </Text>
-                  </Grid>
-                  <Grid cursor="pointer" display="flex">
-                    <Tag>{feed?.location}</Tag>
-                    <Tag>{feed?.distance}km</Tag>
+                    <Grid
+                      width="267px"
+                      height="auto"
+                      bg="white"
+                      borderRadius="6px"
+                      padding="10px 16px"
+                      position="absolute"
+                      left="10px"
+                      bottom="-45px"
+                    >
+                      <Text margin="0 0 4px 0" size="14px" bold>
+                        {feed?.title}
+                      </Text>
+
+                      <Grid
+                        display="flex"
+                        alignItems="center"
+                        margin="10px 0 8px 0"
+                      >
+                        <img
+                          style={{ width: "13px", height: "17px" }}
+                          src={starIcon}
+                        />
+                        <Text margin="0 10px 0 4px" size="13px" bold>
+                          {feed?.starPoint}
+                        </Text>
+                        <Text margin="0" color="#909090" size="12px" regular>
+                          리뷰{feed?.commentCnt}개
+                        </Text>
+                      </Grid>
+                      <Grid cursor="pointer" display="flex">
+                        <Tag>{feed?.location}</Tag>
+                        <Tag>{feed?.distance}km</Tag>
+                      </Grid>
+                    </Grid>
                   </Grid>
                 </Grid>
               </Grid>
-            </Grid>
-          </Grid>
-        )
-        })}
-      </>
-      }     
+            );
+          })}
+        </>
+      )}
     </Grid>
   );
 };
